0abdd1839f4ac64c36a0e6a5106640ed9731ff2d
Daman Bual <damanbual@gmail.com>
2024-11-01 17:42:34 -0700

    created repo clone

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,22 @@
 def benchmark
-  # Your benchmarking code goes here.
+  # Record the start time before executing the block
+  start_time = Time.now
+  
+  # Run the block of code, which is the behavior passed to `benchmark`
+  yield
+  
+  # Record the end time after the block is finished
+  end_time = Time.now
+
+  # Calculate and return the running time in seconds
+  end_time - start_time
 end
  
-# Be careful, pasting this into IRB will take a long time to print.
-# It's a loooong string. :)
-long_string = "apple"*100000000
+# The long string we're going to reverse to test our benchmark
+long_string = "apple" * 100000000
  
+# Benchmark the time it takes to reverse the long string
 running_time = benchmark { long_string.reverse }
  
+# Output the running time
 puts "string.reverse took #{running_time} seconds to run"
